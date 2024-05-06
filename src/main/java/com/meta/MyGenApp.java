1fedb14d0a8fd0f98f090f74f0904dfd34e693f1
Ahmedehab280202 <ahmedehab280202@gmail.com>
2024-05-06 18:09:35 +0300

    MetaMorph Init

---
+++ src/main/java/com/meta/MyGenApp.java
@@ -0,0 +1,12 @@
+package com.meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  MyGenApp{
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGenApp.class, args);
+    }
+}
